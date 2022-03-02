4220d3cc0527d0bee1c166dbadf83003b8821182
adhipramanax <panjiadhipramana@gmail.com>
2022-03-02 23:45:19 +0800

    03-03-22

---
+++ practice_10_deklarasi_tipe_data_output/index.js
@@ -0,0 +1,67 @@
+
+// deklarasi string menggunakan single quote (') dan double quote ("")
+let var_string = "Hello World";
+// typeof untuk mengetahui tipe data
+console.log(typeof(var_string));
+console.log(var_string[6]);
+
+let array_string = ["Hello", "World", "!"];
+console.log(array_string[1][2]);
+
+// deklarasi number
+let var_number = 10;
+console.log(typeof(var_number));
+
+// deklarasi boolean tru dan false
+let var_boolean = false;
+console.log(typeof(var_boolean));
+
+// deklarasi null
+let var_null = null
+console.log(typeof(var_null))
+
+// deklarasi undefined
+let var_undefined = undefined;
+console.log(typeof(var_undefined));
+
+// deklarasi tipe data array #1
+let var_array = ["Hello World", 2, 3 , 4, 5];
+console.log(var_array);
+
+// deklarasi tipe data array #1
+let var_array_new = new Array(1, 2, "Hello World", 4, 5);
+console.log(var_array_new);
+
+// deklarasi object #1
+let var_object = { }
+console.log(var_object);
+
+// deklarasi object #2
+let var_object_new = new Object();
+var_object_new.first = "John";
+var_object_new.last = "Doe"; // cara pemanggilan object #1
+var_object_new["ttl"] = "indonesia, 17 agustus 1945"; // cara pemanggilan object #2
+console.log(var_object_new);
+
+// primitive (number, string, boolean, null, undefined)
+// reference /object (object, array)
+
+
+// deklarasi tipe data 
+// let var_string = "Hello World";
+// let var_number = 10;
+// let var_boolean = false;
+// let var_null = null;
+// let var_undefined = undefined;
+// let var_array = ["Hello World", 2, 3 , 4, 5];
+// let var_object = { }
+
+// menampilkan tipe data
+// console.log(typeof(var_string));
+// console.log(typeof(var_number));
+// console.log(typeof(var_boolean));
+// console.log(typeof(var_null));
+// console.log(typeof(var_undefined));
+// console.log(typeof(var_array));
+// console.log(typeof(var_object));
+
---
+++ practice_11_operator/index.js
@@ -0,0 +1,142 @@
+// + penjumlahan
+// - pengurangan
+// * perkalian
+// / pembagian
+// % modulus
+
+// binary operator 2 operan
+console.log(2 / 3);
+
+// unary operator 1 operan
+console.log(typeof "hello");
+
+// assignment operator 3 operan
+console.log(2 < 3 ? true : false);
+
+// assignment operator
+var x = 10;
+
+// addition assignment 
+x += 10; // a = a + 10
+x -= 10; // a = a - 10
+// logical assignment
+let c = 2 < 3;
+console.log(x);
+
+// comparison operator
+let varA = 10;
+let varB = 30;
+let varC = varA > varB;
+console.log(varC);
+
+// ////////////////////////////////////////////////////////////////////////////////////////////////
+// strict maksud nya tidak harus sama tipe data nya
+// == equal tapi tidak strict
+// != not equal tapi tidak strict
+// === strict equal
+// !== strict not equal
+// < > 
+// <= >=
+
+console.log(10 == "10"); // true
+console.log(10 === "10"); // false
+console.log(10 != "10"); // false
+console.log(10 !== "10"); // true
+
+// ////////////////////////////////////////////////////////////////////////////////////////////////
+// modulus % (sisa dari pembagian bilangan bulat)
+console.log(10 % 3); // 1
+
+// increment dan decrement
+let inc = "4";
+console.log(+inc); // 4
+console.log(-inc); // -4
+
+// pangkat **
+console.log(2 ** 3); // 8
+
+// ////////////////////////////////////////////////////////////////////////////////////////////////
+// Challenge
+// 1. Coba membuat operasi bilangan f(x) = (2x +5) + 100 /25x
+x = 10;
+console.log((2*x +5) + 100 /25*x);
+
+// 2. Coba membuat operasi bilangan komparasi ((kusing === tikus) !== false) // false
+console.log(("kucing" === "tikus") !== false);
+
+// truthy dan falsy
+// && = AND , || = OR , ! = NOT
+console.log(true && !false);
+console.log(true || false);
+console.log(!true);
+
+// console.log(true && false);
+console.log(1 && 0);
+
+// operasi string unik
+let varE = 3 + "3";
+console.log(varE);// 33
+
+// operasi string aneh
+let varF = "3" * 3;
+console.log(varF); // 9
+
+// operasi string lain nya
+var varM = "john";
+var varN = "wikx";
+varM += "naaa";
+varM += "!";
+console.log(varM);// johnnaaa!
+
+// relational operator in
+let arrayA = [10,22,33,34,75];
+console.log(10 in arrayA);// true
+console.log(11 in arrayA);// false
+
+// relational operator in
+let objectZ = {
+    nama: "john wikx"
+}
+console.log("nama" in objectZ);// true
+
+// instanceof
+let date = new Date();
+console.log(date instanceof Date);// true
+
+// binary operator delete
+let objectV = {
+    nama : "John",
+    alamat : "Jakarta",
+    umur : 20}
+    console.log(objectV);// {nama: "John", alamat: "Jakarta", umur: 20}
+delete objectV.alamat;
+console.log(objectV);// {nama: "John", umur: 20}
+
+// /////////////////////////////////////////////////////////////////////////////////////////////////
+// challenge
+
+// membuat perintah untuk melakuikan pengecekan nilau ganjil atau genap cek apakah dia negatif atau positif, nilai yang dibandingkanya tidak lebih dari 100 dan kurang dari -1000 dilarang input 0
+// expected output: ganjil negatif, genap positif, ganjil positif, genap negatif
+
+
+let varA = -101;
+let varB = varA;
+
+if (varA > -1000 && varA < 1000) {
+    if (varA === 0) {
+        console.log('tidak boleh 0');
+    }else if (varA % 2 === 0) {
+        if (varA > 0) {
+            console.log('positif genap');
+        }else if (varA < 0) {
+            console.log('negatif genap');
+        }
+    }else{
+        if (varA > 0) {
+            console.log('positif ganjil');
+        }else if (varA < 0) {
+            console.log('negatif ganjil');
+        }
+    }
+}else{
+    console.log('inputan tidak valid');}
